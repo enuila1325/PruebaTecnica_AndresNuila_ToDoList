4c52ccc1427567a9896596654c6b84827b332cc7
enuila1325 <enuila634@gmail.com>
2023-12-19 19:55:50 -0600

    Crear nuevos quehaceres listo. Escritura y lectura en DB listo.

--- sever.js
+++ sever.js
@@ -14,8 +14,25 @@ mongoose.connect(
   "mongodb+srv://enuila:enuila1325@atlascluster.m2rreiy.mongodb.net/"
 );
 
+// CRUD
+
+// Create
+app.post("/", async (req, res) => {
+  const task = new taskModel({
+    content: req.body.content,
+  });
+  try {
+    await task.save();
+    res.redirect("/");
+  } catch (err) {
+    res.redirect("/");
+  }
+});
+// Read
 app.get("/", function (req, res) {
-  res.render("mainPage.ejs");
+  taskModel.find().then((data) => {
+    res.render("mainPage.ejs", { tareas: data });
+  });
 });
 
 // listening on port 3000
